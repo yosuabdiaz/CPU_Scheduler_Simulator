d8a0c3c464dac6ecc75ab21c5acca34663d847d6
Yosua Blanco <yosuabd@estudiantec.cr>
2022-04-05 00:33:33 -0600

    Sockets client_server

--- client.c
+++ client.c
@@ -6,56 +6,42 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
+#include <string.h>
 
-void main(int argc, char *argv[]){
-    char cadena[100];
+void main(){
+
     int sockfd;
     struct sockaddr_in servaddr;
-    FILE *myf = fopen("Historial_cliente.txt", "a");
-    time_t t; 
-    struct tm *tm;
-    char hora[100];
-    char *tmp;
-    char sendline[100] = "Proceso xD";
-    
-    //Crea el socket para llamar al servidor
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    bzero(&servaddr, sizeof(servaddr));
+    char buffer[1024];
 
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(22000);
-    //Creo que esta IP cambia para cada maquina donde lo corra, revisar
-    inet_pton(AF_INET, "192.168.0.10", &(servaddr.sin_addr));
-    //==============================
-    
-    connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));//llama
-    
-    printf("\n\n\t\t-----------Se inicio el servidor-----------\n\n");
-    fputs("\n\n\t\t-----------Se inicio el servidor-----------\n\n", myf);
+    for (int i=0; i<4; i++){
+        //Crea el socket para llamar al servidor
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        bzero(&servaddr, sizeof(servaddr));
 
-    while(!strstr(cadena, "adios") && !strstr(sendline,"adios")){
-        
-        bzero(cadena, 100);
-        //arma el mensaje
-        t =time(NULL);
-        tm = localtime(&t);
-        strftime(hora, 100, "\n Cliente (%H:%M)->", tm );
-        printf("%s",hora);
-        gets(sendline); //lee de consola
-        tmp = strcat(hora, sendline);//manda el mensaje al dar enter
-        //arma el mensaje
-        fputs(tmp, myf); //archivo
-        write(sockfd, sendline, strlen(sendline)+1);
-        if(!strstr(cadena, "adios")){
-            strftime(hora, 100, "\n Servidor (%H:%M)->", tm);
-            read(sockfd, cadena, 100);
-            tmp = strcat(hora, cadena);
-            printf("%s", tmp);
-            fputs(tmp, myf);
-        }
-        
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(22000);
+        //Creo que esta IP cambia para cada maquina donde lo corra, revisar
+        inet_pton(AF_INET, "192.168.0.10", &(servaddr.sin_addr));
+    
+    
+        connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));//llama
+
+        printf("Connected to server...\n");
+
+        bzero(buffer, 1024);
+        strcpy(buffer, "HELLO, PROCESS 01\n");
+
+        printf("Client: %s \n", buffer);
+        send(sockfd, buffer, strlen(buffer), 0);
     }
-    printf("\n\n Conversacion finalizada \n");
-    printf("\n Se genero el archivo Historial_cliente.txt");
-    fclose(myf);
+    
+
+    bzero(buffer, 1024);
+    recv(sockfd, buffer, sizeof(buffer),0);
+
+    printf("Server: %s \n", buffer);
+
+    close(sockfd);
+    printf("Server Closed\n");
 }
--- server.c
+++ server.c
@@ -6,19 +6,21 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <unistd.h>
+#include <string.h>
 
 void main(int argc, char *argv[]){
     if (argc > 1){ //contador de parametros
-        char cadena[100];
+        //char cadena[100];
         int listen_fd, comm_fd;
         struct sockaddr_in servaddr;
-        FILE *myf = fopen("Historial.txt", "a");
-        time_t t; 
+        //FILE *myf = fopen("Historial.txt", "a");
+        //time_t t; 
         struct tm *tm;
         char hora[100];
         char *tmp;
         char sendline[100] = "Proceso xD";
-
+        char buffer[1024];
+        
         //abrimos el socket
         listen_fd = socket(AF_INET, SOCK_STREAM, 0);
         //se llena el socket con ceros 
@@ -29,56 +31,26 @@ void main(int argc, char *argv[]){
         servaddr.sin_port = htons(22000);//puesto por el que se manda la info
 
         bind(listen_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)); //atender al servicio
+        printf("Listening Client...\n");
         listen(listen_fd, 10);//socket y lista de espera de clientes
-        comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL); //acepta la llamada
-
-        printf("\n\n\t\t-----------Se inicio el servidor-----------\n\n");
-        fputs("\n\n\t\t-----------Se inicio el servidor-----------\n\n", myf);
         
-        char *mode;//captura el parametro para tipo de algoritmo
-        mode = argv[1]; // sacarlo de la lista de parametros
-        while(!strstr(cadena, "adios")&& !strstr(sendline, "adios")){
-            switch (*mode){ // se escoge, no se como sera
-                case 'f':
-                    printf("fifo");
-                    break;
-                
-                case 's':
-                    printf("SJF");
-                    break;
-                
-                case 'h':
-                    printf("HPF");
-                    break;
-                
-                case 'r':
-                    printf("Round Robin");
-                    break;
-
-                default:
-                    printf("tipo no valido");
-                    break;
-            }
-            bzero(cadena, 100);//llena de ceros el socket
-            t = time(NULL);  //hora
-            tm = localtime(&t); //para ver la hora que entro el mensaje
-            strftime(hora, 100, "\n Cliente (%H:%M)->", tm); //Arma el mensaje
+        while (1){
             
-            read(comm_fd, cadena, 100); //leer de la llamada
-            tmp=strcat(hora, cadena); // concatena la info
-            printf("%s", tmp); //consola
-            fputs(tmp, myf);    //escribir archivo
-            if(!strstr(cadena, "adios")){
-                strftime(hora, 100, "\n Servidor (%H:%M)->", tm);
-                printf("%s", hora); //consola
-                gets(sendline); //leer de consola
-                tmp = strcat(hora, sendline); //manda el mensaje al dar enter
-                write(comm_fd, sendline, strlen(sendline)+1);//manda el mensaje
-                fputs(tmp,myf);  //escribir archivo
-            }
-            printf("\n\n\t\t-----------Se termino el servidor-----------\n\n");
-            printf("\n\n\t\t-----------Se genero Historial.txt-----------\n\n");
-        fclose(myf);
+            comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL); //acepta la llamada
+            printf("Cliend Connected\n");
+
+            bzero(buffer, 1024);
+            recv(comm_fd, buffer, sizeof(buffer), 0);
+            printf("New Process : %s\n", buffer);
+
+            bzero(buffer, 1024);
+            strcpy(buffer, "This is server, process end!!!!");
+            printf("Server response: %s \n",buffer);
+            send(comm_fd, buffer, strlen(buffer), 0);
+
+            close(comm_fd);
+            printf("Client closed\n");
+
         }
     }else{
         printf("Parametro esperdado \n");
